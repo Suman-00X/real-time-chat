8ae408a050e73ee31fa2a353758072b4e8faaa0e
Suman raj <raj_suman@hotmail.com>
2024-05-22 14:27:22 +0530

    reconfigure package.json for deployement

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "https://chat-backend-eewg.onrender.com",
+        target: "http:localhost:7484",
         changeOrigin: true,
         secure: false,
       },
